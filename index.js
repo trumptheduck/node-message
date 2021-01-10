3e1ce2fb086fb974dbe9d3105fe4a4a0b5062336
unknown <nhatyt123@gmail.com>
2021-01-11 01:26:43 +0700

    Open Beta

--- index.js
+++ index.js
@@ -2,25 +2,14 @@ const app = require('express')();
 const http = require('http').Server(app);
 const path = require("path")
 const io = require('socket.io')(http);
-const users = [
-  {
-    username: "nhatyt123",
-    password: "nhat1234",
-    roomcode: '20205201314',
-  },
-  {
-    username: "hanghang1323",
-    password: "hang1234",
-    roomcode: '20205201314',
-  }
-]
-const rooms = ['20205201314']
-let chatData = [
-  {
-    roomcode: '20205201314',
-    data:[]
-}
-]
+const fs = require('fs');
+let usersRawdata = fs.readFileSync('./data/users.json');
+let roomsRawdata = fs.readFileSync('./data/rooms.json');
+let chatDataRawdata = fs.readFileSync('./data/chatData.json');
+let users = JSON.parse(usersRawdata);
+let rooms = JSON.parse(roomsRawdata);
+let chatData = JSON.parse(chatDataRawdata);
+console.log(users)
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,'index.html'));
 })
@@ -34,7 +23,7 @@ io.on('connection', (socket) => {
         } else {
           if(user.password === data.password) {
             console.log("Login successful");
-            socket.emit('loginstate',user.roomcode)
+            socket.emit('loginstate',true)
             let chat = chatData.find(chat => chat.roomcode = user.roomcode);
             socket.emit('getMessage',chat)
             socket.on('message',(msg)=>{
@@ -43,6 +32,10 @@ io.on('connection', (socket) => {
                 message: msg,
               })
               console.log(chat)
+              socket.emit('getMessage',chat)
+            })
+            
+            socket.on('update', ()=>{
               socket.emit('getMessage',chat)
             })
           } else {
@@ -56,7 +49,15 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
       });
   });
-  
+  setInterval(()=>{
+    let chatDataData = JSON.stringify(chatData);
+    let roomsData = JSON.stringify(rooms);
+    let usersData = JSON.stringify(users);
+    fs.writeFileSync('./data/chatData.json', chatDataData); 
+    fs.writeFileSync('./data/rooms.json', roomsData); 
+    fs.writeFileSync('./data/users.json', usersData); 
+    console.log("Data saved!")
+  },10000)
 http.listen(3000, () => {
     console.log('listening on *:3000');
   });
